599691573771e0aa44c7aa6fbf4dee3de6f65b97
pingmong <kny7024@naver.com>
2024-01-17 21:02:27 +0900

    버튼 및 줄 애니메이션 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,33 +1,33 @@
 import "./TodoItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-regular-svg-icons";
-import {useState} from "react";
-import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+// import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
-const TodoItem = ({ id, content, isDone, createdDate, onUpdate, onDelete}) => {
-    const [check, setCheck] = useState(false);
+const TodoItem = ({ id, content, isDone, createdDate, onUpdate, onDelete }) => {
+    const [check, setCheck] = useState(isDone);
+
     const onClickCheckbox = () => {
         onUpdate(id);
         setCheck(!check);
-
     };
 
     const onClickDelete = () => {
         onDelete(id);
-    }
+    };
 
     return (
-        <div className="TodoItem">
-            <div className="checkbox_col" onClick={onClickCheckbox}
-                 checked={isDone}>
-                {check ? (<FontAwesomeIcon icon={faCheck} style={{color: "#526be0",}} />) :(<FontAwesomeIcon icon={faCircle} style={{color: "#526be0",}} />)}
+        <div className={`TodoItem`}>
+            <div className={`checkbox_col ${check ? 'active' : ''}`} onClick={onClickCheckbox}>
+                {check && <FontAwesomeIcon icon={faCheck} />}
             </div>
-            <div className="title_col">{ content }</div>
-            <div className="date_col">{new Date().toLocaleDateString()}</div>
+            <div className="title_col">{check ? <p className="animated-text">{content}</p> : <p>{content}</p>}</div>
+            <div className="date_col">{new Date(createdDate).toLocaleDateString()}</div>
             <div className="btn_col">
                 <button onClick={onClickDelete}>삭제</button>
             </div>
         </div>
     );
 };
+
 export default TodoItem;
