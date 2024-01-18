e85bc9d7a76727099c5abb1a94d92640fffc4dc9
yenn222 <20221038@o365.hanbat.ac.kr>
2024-01-18 18:59:04 +0900

    feat: 버튼과 input 수정

--- src/components/TodoEditor.js
+++ src/components/TodoEditor.js
@@ -1,9 +1,27 @@
 import "./TodoEditor.css";
 import {useRef, useState} from "react";
-import Inputbtn from "./Inputbtn";
 
 const TodoEditor = ({ onCreate, mockTodo }) => {
     const [btnclick, setBtnclick] = useState(false);
+    const [content, setContent] = useState("");
+    const inputRef = useRef();
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    }
+    const onKeyDown = (e) => {
+        if(e.keyCode === 13){
+            onSubmit();
+        }
+    }
+    const onSubmit = () => {
+        if (!content) {
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+        setBtnclick(false);
+    };
 
     const onInput = () => {
         setBtnclick(!btnclick);
@@ -11,10 +29,20 @@ const TodoEditor = ({ onCreate, mockTodo }) => {
 
     return (
         <div className="TodoEditor">
-            <div className="editor_wrapper">
-                {btnclick ? <Inputbtn />: null}
-                <button  onClick={onInput}>+</button>
-            </div>
+                    { btnclick ? (
+                        <div className="editor_wrapper">
+                        <input
+                        ref={inputRef}
+                        value={content}
+                        onChange={onChangeContent}
+                        onKeyDown={onKeyDown}
+                        placeholder="새로운 Todo..."/>
+                    <button onClick={onSubmit}>추가</button>
+                        </div>) : (<div className="editor_wrapper_input">
+                            <button onClick={onInput}>+</button>
+                        </div>
+                    )}
+
         </div>
     );
 };
